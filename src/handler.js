d0df5f3bfbfe80905ba0c8e0d293b2dfc98241a8
Dedy Irama <dedyirama.ars@gmail.com>
2024-07-09 14:28:27 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,130 @@
+const { nanoid } = require('nanoid');
+const albums = require('./albums');
+
+const addAlbumHandler = (request, h) => {
+  const { title = 'untitled', tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newAlbum = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  albums.push(newAlbum);
+
+  const isSuccess = albums.filter((album) => album.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        albumId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllAlbumsHandler = () => ({
+  status: 'success',
+  data: {
+    albums,
+  },
+});
+
+const getAlbumByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const album = albums.filter((n) => n.id === id)[0];
+
+  if (album !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        album,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const editAlbumByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = albums.findIndex((album) => album.id === id);
+
+  if (index !== -1) {
+    albums[index] = {
+      ...albums[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteAlbumByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = albums.findIndex((album) => album.id === id);
+
+  if (index !== -1) {
+    albums.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addAlbumHandler,
+  getAllAlbumsHandler,
+  getAlbumByIdHandler,
+  editAlbumByIdHandler,
+  deleteAlbumByIdHandler,
+};
